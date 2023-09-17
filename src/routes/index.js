be29c687151aa193705e4c4afef116eb9b0d7f92
Juan Patricio <ggjuanpatricio@gmail.com>
2023-09-16 21:34:53 -0300

    controllersv1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const getPrueba = require("../handlers/getPrueba");
+const createOrganizer = require("../handlers/organizer/POST/HandlerCreateOrganizer.js");
 
 const routes = Router();
+routes.post('/organizer/create', createOrganizer)
 
 routes.get('/', getPrueba)
 
